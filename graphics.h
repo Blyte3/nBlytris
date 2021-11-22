24e7edfe0c585051c367662e7a856096d6ed3946
Blyte3 <blyte3@protonmail.com>
2021-11-21 17:42:21 -0800

    hold and queue are displayed visually, changes in PrintStack

--- graphics.h
+++ graphics.h
@@ -1,13 +1,69 @@
 #include <ncurses.h>
 
+const char piecechars[8]={"IJLOSTZ"};
+int rf;
+
+//ijlostz order. pieces to be printed out in queue
+int PieceImage[7][2][4]={
+	{{1,1,1,1},
+	 {0,0,0,0}},
+	 
+	{{1,0,0,0},
+	 {1,1,1,0}},
+	
+	{{0,0,1,0},
+	 {1,1,1,0}},
+	
+	{{1,1,0,0},
+	 {1,1,0,0}},
+	
+	{{0,1,1,0},
+	 {1,1,0,0}},
+	
+	{{0,1,0,0},
+	 {1,1,1,0}},
+	
+	{{1,1,0,0},
+	 {0,1,1,0}}
+};
+
+void PrintQueuePiece(){
+
+	if(r%3){
+	
+		for(rth=0;rth<4;rth++){
+	
+			if(PieceImage[queue[queueaccesspoint+rf]][(3-(r%3))-1][rth]==1){printw("X ");}
+			else{printw("  ");}
+		}
+	}
+	else{rf++;}
+}
+	 
 //maybe make a seperate i and it instead of r and rt
 void PrintStack(){
 	
-    printw("\n");
+	rf=1;
+    printw("\n          ____________________\n");
     
-    for (r=24;r>-1;r--){
+    for(r=20;r>-1;r--){
+    	
+    	//print the hold piece
+    	if((r==20 || r==19) && holdpiece>-1){
+    		
+    		printw(" ");
+    		for(rth=0;rth<4;rth++){
+    		
+    			if(PieceImage[holdpiece][3-(r%3)-1][rth]){printw("X ");}
+    			else{printw("  ");}
+    		}	
+    	}
+    	else{printw("         ");}
+    	
+    	
     	printw("|");
-        for (rt=0;rt<10;rt++){
+    	//print the actual board
+        for(rt=0;rt<10;rt++){
         	
         	switch(board[rt][r]){
         	
@@ -20,11 +76,13 @@ void PrintStack(){
         		default:
         			printw("x ");
         	}
-        	//printw("%d ",board[rt][r]);
         }
         
-        printw("|\n");
+        printw("| ");
+        PrintQueuePiece();
+        printw("\n");
     }
+    printw("          --------------------");
 }
 
 void PrintActive(){
@@ -37,10 +95,7 @@ void RemoveActive(){
 	for(r=0;r<4;r++){board[piecex[r]][piecey[r]]=0;}
 }
 
-void PrintQueueAndHold(){
 
-	for(r=0;r<7;r++){printw("%c ",piecechars[queue[queueaccesspoint+r]]);}
-		
-	printw("  hold:%c",piecechars[holdpiece]);
-	printw("\n");
-}
+
+
+
