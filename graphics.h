5d591bcbe70f0ed8fe9b3c174b58f566577bb88b
Blyte3 <blyte3@protonmail.com>
2021-12-10 21:14:32 -0800

    color added

--- graphics.h
+++ graphics.h
@@ -23,6 +23,21 @@ int PieceImage[7][2][4]={
 	{{1,1,0,0},
 	 {0,1,1,0}}
 };
+
+void ChangeColor(int piece){
+
+	switch(piece){
+	
+		case 1:attron(COLOR_PAIR(1)); break;
+		case 2:attron(COLOR_PAIR(2)); break;
+		case 3:attron(COLOR_PAIR(3)); break;
+		case 4:attron(COLOR_PAIR(4)); break;
+		case 5:attron(COLOR_PAIR(5)); break;
+		case 6:attron(COLOR_PAIR(6)); break;
+		case 7:attron(COLOR_PAIR(7)); break;
+		default:attron(COLOR_PAIR(8)); break;
+	}
+}
 	 
 //maybe make a seperate i and it instead of r and rt
 void PrintStack(){
@@ -30,7 +45,7 @@ void PrintStack(){
 	int r,rt,rth;
 	
 	rth=1;
-	
+	ChangeColor(0);
     printw("\n          ____________________\n");
     
     for(r=20;r>-1;r--){
@@ -42,12 +57,21 @@ void PrintStack(){
     		
     		for(rt=0;rt<4;rt++){
     		
-    			if(PieceImage[holdpiece][3-(r%3)-1][rt]){printw("X ");}
-    			else{printw("  ");}
+    			if(PieceImage[holdpiece][3-(r%3)-1][rt]){
+    				
+    				ChangeColor(holdpiece+1);
+    				printw("X ");
+    			}
+    			else{
+    				
+    				ChangeColor(0);
+    				printw("  ");
+    			}
     		}	
     	}
     	else{printw("         ");}
     	
+    	ChangeColor(0);
     	printw("|");
     	
     	//prints board
@@ -55,33 +79,45 @@ void PrintStack(){
         	
         	switch(board[rt][r]){
         	
-        		case 1:
-        			printw("O ");
-        			break;
         		case 0:
+        			ChangeColor(0);
         			printw(". ");
         			break;
         		default:
+        			ChangeColor(board[rt][r]);
         			printw("x ");
         	}
         }
         
+        ChangeColor(0);
         printw("| ");
         
         //prints the piece in the queue
         if(r%3){
 	
+			
+	
 			for(rt=0;rt<4;rt++){
 	
-				if(PieceImage[queue[queueaccesspoint+rth]][(3-(r%3))-1][rt]==1){printw("X ");}
-				else{printw("  ");}	
+				if(PieceImage[queue[queueaccesspoint+rth]][(3-(r%3))-1][rt]==1){
+					
+					ChangeColor(queue[queueaccesspoint+rth]+1);
+					printw("X ");
+				}
+				else{
+					
+					ChangeColor(0);
+					printw("  ");
+				}	
 			}
 		}
 		else{rth++;}
 		
+		ChangeColor(0);
         printw("\n");
     }
     
+    ChangeColor(0);
     printw("          --------------------");
 }
 
@@ -89,7 +125,7 @@ void PrintActive(){
 	
 	int r;
 	
-	for(r=0;r<4;r++){board[piecex[r]][piecey[r]]=2;}
+	for(r=0;r<4;r++){board[piecex[r]][piecey[r]]=activepiece+1;}
 }
 
 void RemoveActive(){
@@ -99,7 +135,22 @@ void RemoveActive(){
 	for(r=0;r<4;r++){board[piecex[r]][piecey[r]]=0;}
 }
 
+void InitGraphics(){
 
-
-
-
+	initscr();
+	
+	start_color();
+	init_color(COLOR_WHITE,850,850,100);
+	init_pair(7,COLOR_RED,COLOR_RED);
+	init_pair(4,COLOR_WHITE,COLOR_WHITE);
+	init_pair(3,COLOR_YELLOW,COLOR_YELLOW);
+	init_pair(5,COLOR_GREEN,COLOR_GREEN);
+	init_pair(2,COLOR_BLUE,COLOR_BLUE);
+	init_pair(1,COLOR_CYAN,COLOR_CYAN);
+	init_pair(6,COLOR_MAGENTA,COLOR_MAGENTA);
+	init_pair(8,COLOR_CYAN,COLOR_BLACK);
+	
+	PrintActive();
+	PrintStack();
+	refresh();
+}
